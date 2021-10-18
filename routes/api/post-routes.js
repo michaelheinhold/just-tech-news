47b0f82e0b575c198ca06cf8ef6e592486f6ef0e
DerVerruckte <michael.heinhold@gmail.com>
2021-10-18 12:24:19 -0600

    added comment model and routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize =require('../../config/connection');
 
 router.get('/', (req, res) => {
@@ -13,6 +13,14 @@ router.get('/', (req, res) => {
             {
                 model: User,
                 attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User, 
+                    attributes: ['username']
+                }
             }
         ]
     }).then(dbPostData => res.json(dbPostData))
@@ -34,6 +42,14 @@ router.get('/:id', (req, res) => {
             {
                 model: User,
                 attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
             }
         ]
     })
